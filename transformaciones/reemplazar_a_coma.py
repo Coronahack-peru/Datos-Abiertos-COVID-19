870008c696ea4fc6364a8801f28b213f07cbcfec
Edwin Gonzales <edwinpgm@gmail.com>
2020-05-23 15:52:56 -0500

    Cambiar los separadores con coma (,) y en UTF-8 usando pandas

--- transformaciones/reemplazar_a_coma.py
+++ transformaciones/reemplazar_a_coma.py
@@ -1,10 +1,9 @@
 import sys
 
+import pandas as pd
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-text = open(input_file, "r")
-text = ''.join([i for i in text]).replace(";", ",")
-x = open(output_file,"w")
-x.writelines(text)
-x.close()
+df = pd.read_csv(input_file, delimiter=';', encoding="ISO-8859-1")
+df.to_csv(output_file, encoding="UTF-8", index=False)
