212fb8b18b337779cd03089c6c1dd1916c69eaa7
Javier Caparo <javier.caparo@gmail.com>
2020-05-23 15:46:15 -0500

    scripts updated 15:46 hpurs

--- convertcsv-to-utf8-encoding.py
+++ convertcsv-to-utf8-encoding.py
@@ -12,6 +12,7 @@
 
 import os    
 import chardet
+import shutil
 import pandas as pd
 import numpy as np
 
@@ -32,27 +33,33 @@ def get_encoding_type(file):
 
 
 # change file encoding function
-def change_file_encoding(filename):
+def change_file_encoding(filename,encoded_dir):
     srcfile=filename
     prefix='fixed'
-    trgfile=prefix +srcfile
+    trgfile=prefix + srcfile
+    new_dir= encoded_dir
     from_codec = get_encoding_type(srcfile)
     print("Archivo :",filename, "Encoding:", from_codec)
     if from_codec != 'utf-8':
         try: 
-            print("Convirtiendo a utf-8")
+            print("Convirtiendo a utf-8 y copiando al directorio data_limpia")
             with open(srcfile, 'r', encoding=from_codec) as f, open(trgfile, 'w', encoding='utf-8') as e:
                 text = f.read() # for small files, for big use chunks
                 e.write(text)
-
-            os.remove(srcfile) # remove old encoding file
-            os.rename(trgfile, srcfile) # rename new encoding
+                
+            shutil.copy2(trgfile, encoded_dir)
+            os.chdir(new_dir)
+            os.rename(trgfile, srcfile)
+#            os.remove(srcfile) # remove old encoding file
+#            os.rename(trgfile, srcfile) # rename new encoding
         except UnicodeDecodeError:
             print('Decode Error')
         except UnicodeEncodeError:
             print('Encode Error')
     else:
-        print('encoding is already utf-8. No cambio')
+        print('encoding is already utf-8. No cambio. Se  copio al directorio data_limpia')
+        shutil.copy2(srcfile, encoded_dir)
+    
     return
 
 
@@ -69,9 +76,11 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
 # In[5]:
 
 
-filenames = find_csv_filenames("./")
+base_dir = "./"
+encoded_dir = "./data_limpia"
+filenames = find_csv_filenames(base_dir)
 for name in filenames:
   print ("Trabajando con archivo:",name)
-  change_file_encoding(name)
+  change_file_encoding(name,encoded_dir)
 print("Terminado")
 
